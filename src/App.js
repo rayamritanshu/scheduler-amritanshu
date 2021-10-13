a1c577bb565fcafcba4756782554cc0f83f887ba
rayamritanshu <amritanshuray2022@u.northwestern.edu>
2021-10-12 22:28:00 -0500

    Storing changed class times

--- src/App.js
+++ src/App.js
@@ -2,69 +2,20 @@ import React from 'react';
 import './App.css';
 import { CourseList } from './components/CourseList'
 import { useData } from './utilities/firebase.js';
-
-/*
-const schedule = {
-  "title": "CS Courses for 2020-2021",
-  "courses": {
-    "F101" : {
-      "id" : "F101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "id" : "F110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "id" : "S313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "id" : "S314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
-*/
+import { addScheduleTimes } from './utilities/times';
 
 const Banner = ({ title }) => (
   <h1>{ title }</h1>
 );
 
-const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
 
 export const toggle = (x, lst) => (
   lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
 );
 
-const timeParts = meets => {
-  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
-  return !match ? {} : {
-    days,
-    hours: {
-      start: hh1 * 60 + mm1 * 1,
-      end: hh2 * 60 + mm2 * 1
-    }
-  };
-};
 
-const mapValues = (fn, obj) => (
-  Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
-);
 
-const addCourseTimes = course => ({
-  ...course,
-  ...timeParts(course.meets)
-});
-
-const addScheduleTimes = schedule => ({
-  title: schedule.title,
-  courses: mapValues(addCourseTimes, schedule.courses)
-});
 
 const App = () => {
   const [schedule, loading, error] = useData('/', addScheduleTimes); 
--- src/utilities/times.js
+++ src/utilities/times.js
@@ -30,3 +30,31 @@ const daysOverlap = (days1, days2) => (
 const hoursOverlap = (hours1, hours2) => (
     Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
   );
+
+export const timeParts = meets => {
+    const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+    return !match ? {} : {
+      days,
+      hours: {
+        start: hh1 * 60 + mm1 * 1,
+        end: hh2 * 60 + mm2 * 1
+      }
+    };
+  };
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+export const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses)
+});
+
+
+const mapValues = (fn, obj) => (
+  Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
+);
+
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
